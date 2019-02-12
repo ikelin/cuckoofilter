3a4bb21c86d14e16ecd5466a7f17c62bd91aa169
Ike Lin <ikelin@users.noreply.github.com>
2019-02-11 19:48:31 -0800

    remove concurrency test

--- src/test/java/com/ikelin/cuckoofilter/CuckooFilterTest.java
+++ src/test/java/com/ikelin/cuckoofilter/CuckooFilterTest.java
@@ -5,18 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -247,113 +235,6 @@ class CuckooFilterTest {
     assertTrue(filter.remove(fooHash));
     assertEquals(0, filter.getItems());
 
-  }
-
-  @Test
-  void testConcurrency()
-      throws ExecutionException, InterruptedException, IOException, URISyntaxException {
-    int capacity = (int) ((1 << 10) * 0.9D);
-    int items = (int) (capacity * 0.9D);
-    double fpp = 0.002D;
-
-    CuckooFilter filter = CuckooFilter.create(capacity)
-        .withFalsePositiveProbability(fpp)
-        .withConcurrencyLevel(8)
-        .build();
-
-    ExecutorService executorService = Executors.newFixedThreadPool(8);
-
-    // load banned ids
-    List<Long> bannedIds = new ArrayList<>();
-    try (Stream<String> lines = Files
-        .lines(Paths.get(ClassLoader.getSystemResource("banned-ids.txt").toURI()))) {
-      lines.forEach(line -> bannedIds.add(Long.parseLong(line)));
-    }
-
-    // load request ids
-    List<Long> requestIds = new ArrayList<>();
-    try (Stream<String> lines = Files
-        .lines(Paths.get(ClassLoader.getSystemResource("request-ids.txt").toURI()))) {
-      lines.forEach(line -> requestIds.add(Long.parseLong(line)));
-    }
-
-    // put banned ids into set while querying request ids
-    List<Future<Boolean>> bannedIdFutures = new ArrayList<>();
-    List<Future<Boolean>> requestIdFutures = new ArrayList<>();
-
-    for (int i = 0; i < bannedIds.size(); i++) {
-      final int index = i;
-      Future<Boolean> bannedIdFuture = executorService
-          .submit(() -> filter.put(bannedIds.get(index)));
-      bannedIdFutures.add(bannedIdFuture);
-
-      Future<Boolean> requestIdFuture = executorService
-          .submit(() -> filter.mightContain(requestIds.get(index)));
-      requestIdFutures.add(requestIdFuture);
-    }
-
-    // all puts should be successful
-    for (Future<Boolean> future : bannedIdFutures) {
-      assertTrue(future.get());
-    }
-    bannedIdFutures.clear();
-
-    // non of the request ids should be in set
-    for (Future<Boolean> future : requestIdFutures) {
-      assertFalse(future.get());
-    }
-    requestIdFutures.clear();
-
-    // verify all banned ids are inserted
-    assertEquals(items, filter.getItems());
-    assertEquals((double) filter.getItems() / (filter.getCapacity()), filter.getLoadFactor(),
-        0.001);
-
-    // banned ids return true for might contain
-    for (long id : bannedIds) {
-      Future<Boolean> future = executorService.submit(() -> filter.mightContain(id));
-      bannedIdFutures.add(future);
-    }
-
-    for (Future<Boolean> future : bannedIdFutures) {
-      assertTrue(future.get());
-    }
-    bannedIdFutures.clear();
-
-    // request ids return false for might contain
-    for (long id : requestIds) {
-      Future<Boolean> future = executorService.submit(() -> filter.mightContain(id));
-      requestIdFutures.add(future);
-    }
-
-    for (Future<Boolean> future : requestIdFutures) {
-      assertFalse(future.get());
-    }
-    requestIdFutures.clear();
-
-    // remove banned ids while querying request ids
-    for (int i = 0; i < bannedIds.size(); i++) {
-      final int index = i;
-      Future<Boolean> bannedIdFuture = executorService
-          .submit(() -> filter.remove(bannedIds.get(index)));
-      bannedIdFutures.add(bannedIdFuture);
-
-      Future<Boolean> requestIdFuture = executorService
-          .submit(() -> filter.mightContain(requestIds.get(index)));
-      requestIdFutures.add(requestIdFuture);
-    }
-
-    for (Future<Boolean> future : bannedIdFutures) {
-      assertTrue(future.get());
-    }
-    bannedIdFutures.clear();
-    assertEquals(0, filter.getItems());
-
-    // non of the request ids should be in set
-    for (Future<Boolean> future : requestIdFutures) {
-      assertFalse(future.get());
-    }
-    requestIdFutures.clear();
   }
 
 }
